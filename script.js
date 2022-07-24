b94a7ef241d23b83e01a80e654a253a91264b60b
florian <florian.batt@hotmail.fr>
2022-07-24 17:49:12 +0200

    locate false 30%

--- script.js
+++ script.js
@@ -1,77 +1,85 @@
-startQuagga();
+// startQuagga();
 
-function startQuagga() {
-  const constraints = {
-    width: {
-      min: 640,
-    },
-    height: {
-      min: 480,
-    },
-    aspectRatio: {
-      min: 1,
-      max: 2,
-    },
-    focusMode: "continuous",
-  };
+// function startQuagga() {
+//   const constraints = {
+//     width: {
+//       min: 640,
+//     },
+//     height: {
+//       min: 480,
+//     },
+//     aspectRatio: {
+//       min: 1,
+//       max: 2,
+//     },
+//     focusMode: "continuous",
+//   };
 
-  Quagga.init(
-    {
-      config: {
-        numOfWorkers: 4,
-        locate: false,
-        frequency: 10,
-        debug: {
-          showCanvas: true,
-          showPatches: true,
-          showFoundPatches: true,
-          showSkeleton: true,
-          showLabels: true,
-          showPatchLabels: true,
-          showRemainingPatchLabels: true,
-          boxFromPatches: {
-            showTransformed: true,
-            showTransformedBox: true,
-            showBB: true,
-          },
+Quagga.init(
+  {
+    config: {
+      numOfWorkers: 4,
+      locate: false,
+      frequency: 10,
+      debug: {
+        showCanvas: true,
+        showPatches: true,
+        showFoundPatches: true,
+        showSkeleton: true,
+        showLabels: true,
+        showPatchLabels: true,
+        showRemainingPatchLabels: true,
+        boxFromPatches: {
+          showTransformed: true,
+          showTransformedBox: true,
+          showBB: true,
         },
       },
-      inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        target: document.querySelector("#camera"), // Or '#yourElement' (optional)
-      },
-      decoder: {
-        readers: ["ean_reader"],
-      },
-      locator: {
-        patchSize: "medium",
-        halfSample: true,
-      },
-      area: {
-        top: "25%",
-        right: "0%",
-        left: "0%",
-        bottom: "25%",
-      },
     },
-    function (err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("Initialization finished. Ready to start");
-      Quagga.start();
+    inputStream: {
+      name: "Live",
+      type: "LiveStream",
+      target: document.querySelector("#camera"), // Or '#yourElement' (optional)
+    },
+    decoder: {
+      readers: ["ean_reader"],
+    },
+    locate: false,
+    area: {
+      // defines rectangle of the detection/localization area
+      top: "30%", // top offset
+      right: "30%", // right offset
+      left: "30%", // left offset
+      bottom: "30%", // bottom offset
+    },
+    locator: {
+      patchSize: "medium",
+      halfSample: true,
+    },
+    area: {
+      top: "25%",
+      right: "0%",
+      left: "0%",
+      bottom: "25%",
+    },
+  },
+  function (err) {
+    if (err) {
+      console.log(err);
+      return;
     }
-  );
-  const getresult = document.getElementById("result");
+    console.log("Initialization finished. Ready to start");
+    Quagga.start();
+  }
+);
+const getresult = document.getElementById("result");
 
-  console.log(getresult);
-  Quagga.onDetected(function (data) {
-    console.log(data);
-    getresult.innerHTML = data.codeResult.code;
-  });
-}
+console.log(getresult);
+Quagga.onDetected(function (data) {
+  console.log(data);
+  getresult.innerHTML = data.codeResult.code;
+});
+// }
 
 // if (!("BarcodeDetector" in window)) {
 //   console.log("Barcode Detector is not supported by this browser.");
